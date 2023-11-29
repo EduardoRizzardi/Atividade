071dd33bf3f3823625393c4a3de17013e3dfc771
EduardoRizzardi <edu.cappellari.rizzardi@gmail.com>
2023-11-29 20:13:41 -0300

    Meu primeiro commit

---
+++ src/eduardoavaliacaogit/EduardoAvaliacaoGit.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eduardoavaliacaogit;
+
+/**
+ *
+ * @author 182200153
+ */
+public class EduardoAvaliacaoGit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
